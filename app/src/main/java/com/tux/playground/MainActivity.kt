1f500854c14a7805eb98f2a968b13124daf577d9
Hung Vu <vuhung3990@gmail.com>
2018-09-11 15:53:15 +0700

    open intent activity when known package
    you can see package by: adb dumpsys top

--- app/src/main/java/com/tux/playground/MainActivity.kt
+++ app/src/main/java/com/tux/playground/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.tux.playground
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+  private val PREF_NAME: String = "file_pref"
+  private val KEY: String = "kk"
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    startActivity(Intent().setClassName("com.evenwell.powersaving.g3","com.evenwell.powersaving.g3.exception.PowerSaverExceptionActivity"))
   }
 }
