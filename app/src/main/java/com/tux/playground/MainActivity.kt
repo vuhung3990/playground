2f13b14a21414d84b10ac07baf46d2c792332afb
Hung Vu <vuhung3990@gmail.com>
2018-10-05 15:04:40 +0700

    simple calculator

--- app/src/main/java/com/tux/playground/MainActivity.kt
+++ app/src/main/java/com/tux/playground/MainActivity.kt
@@ -2,11 +2,19 @@ package com.tux.playground
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    button.setOnClickListener {
+      val n1 = editText.text.toString().toInt()
+      val n2 = editText2.text.toString().toInt()
+      val result = n1 + n2
+      textView2.text = result.toString()
+    }
   }
 }
