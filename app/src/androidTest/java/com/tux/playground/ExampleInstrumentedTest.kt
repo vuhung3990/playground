cd5c2b0aed0327a18d78030f9d8c99ece698e007
Hung Vu <vuhung3990@gmail.com>
2018-11-07 15:29:37 +0700

    offline first v2

--- app/src/androidTest/java/com/tux/playground/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/tux/playground/ExampleInstrumentedTest.kt
@@ -8,6 +8,7 @@ import com.tux.playground.database.User
 import com.tux.playground.database.UserDao
 import io.reactivex.Maybe
 import io.reactivex.Observable
+import io.reactivex.Single
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -20,71 +21,67 @@ import org.junit.runner.RunWith
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
-    private lateinit var mUserDao: UserDao
-    private lateinit var mDb: DatabaseApp
-    @Before
-    fun setUp() {
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        mDb = Room.inMemoryDatabaseBuilder(appContext, DatabaseApp::class.java).build()
-        mUserDao = mDb.userDao
-    }
 
-    @Test
-    fun testInsertDb() {
-        val firstName = "peter"
-        val lastName = "tux"
-        val user = User(0, firstName, lastName)
+  private lateinit var mUserDao: UserDao
+  private lateinit var mDb: DatabaseApp
+  @Before
+  fun setUp() {
+    val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+    mDb = Room.inMemoryDatabaseBuilder(appContext, DatabaseApp::class.java).build()
+    mUserDao = mDb.userDao
+  }
 
-        Observable
-                // insert data
-                .fromCallable { mUserDao.insert(user) }
-                // get data
-                .flatMapSingle { mUserDao.findUserByFirstName(firstName) }
-                .test()
-                .assertValue {
-                    it[0].firstName == firstName && it[0].lastName == lastName
-                }
-    }
+  @Test
+  fun testInsertDb() {
+    val firstName = "peter"
+    val lastName = "tux"
+    val user = User(0, firstName, lastName)
 
-    @Test
-    fun offlineFirst() {
-        val listUser = listOf(
-                User(firstName = "hung", lastName = "vu"),
-                User(firstName = "peter", lastName = "tux"),
-                User(firstName = "hieu", lastName = "nguyen")
-        )
+    Observable
+        // insert data
+        .fromCallable { mUserDao.insert(user) }
+        // get data
+        .flatMapSingle { mUserDao.findUserByFirstName(firstName) }
+        .test()
+        .assertValue {
+          it[0].firstName == firstName && it[0].lastName == lastName
+        }
+  }
 
-        // for case 2
-        // mUserDao.insert(listUser)
-        // case1 no data
-        // 1. get data from db
-        mUserDao.findUsers()
-                .filter {
-                    val shouldRequestApi = it.isEmpty()
+  @Test
+  fun offlineFirst() {
+    val listUser = listOf(
+        User(firstName = "hung", lastName = "vu"),
+        User(firstName = "peter", lastName = "tux"),
+        User(firstName = "hieu", lastName = "nguyen")
+    )
 
-                    // case2 data exists => just take it
-                    if (!shouldRequestApi) {
-                        println("case 2: have data")
-                        println(it.toString())
-                    }
-                    return@filter shouldRequestApi
-                }
-                // 2. query api -> parse to list object
-                // 3. save into db
-                .flatMap {
-                    Maybe.fromCallable { mUserDao.insert(listUser) }
-                }
-                .flatMapSingle {
-                    mUserDao.findUsers()
-                }
-                .test()
-                // 3 item from db
-                .assertValue {
-                    println("case 1: no data")
-                    println(it.toString())
-                    it.size == 3
-                }
+    // for case 2
+     mUserDao.insert(listUser)
+    // case1 no data
+    // 1. get data from db
+    mUserDao.findUsers()
+        .flatMap { currentData ->
+          val shouldRequestApi = currentData.isEmpty()
 
-        // case exist data
-    }
+          // case2 data exists => just take it
+          if (!shouldRequestApi) {
+            Observable.just(currentData).singleOrError()
+          } else {
+            // 2. query api -> parse to list object
+            // 3. save into db
+            Maybe.fromCallable { mUserDao.insert(listUser) }
+                .flatMapSingle { mUserDao.findUsers() }
+          }
+        }
+        .test()
+        // 3 item from db
+        .assertValue {
+          println("case 1: no data")
+          println(it.toString())
+          it.size == 3
+        }
+
+    // case exist data
+  }
 }
