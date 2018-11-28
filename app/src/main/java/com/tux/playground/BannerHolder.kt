75a8e5e088df18263181bf465a4fd2b43cbc6139
Hung Vu <vuhung3990@gmail.com>
2018-11-28 13:56:01 +0700

    restore last position viewpager

--- app/src/main/java/com/tux/playground/BannerHolder.kt
+++ app/src/main/java/com/tux/playground/BannerHolder.kt
@@ -5,7 +5,10 @@ import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.item_banner.view.*
 
 class BannerHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-  fun bind(data: List<String>) {
+  fun bind(data: List<String>, lastBannerIndex: Int) {
     itemView.banner_viewpager.adapter = BannerAdapter(data)
+    itemView.banner_viewpager.currentItem = lastBannerIndex
   }
+
+  fun lastIndex() = itemView.banner_viewpager.currentItem
 }
--- app/src/main/java/com/tux/playground/SimpleListAdapter.kt
+++ app/src/main/java/com/tux/playground/SimpleListAdapter.kt
@@ -4,9 +4,20 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
+const val BANNER_POSITION = 0
+const val VIEWTYPE_BANNER = 0
+const val VIEWTYPE_ITEM = 1
+
 class SimpleListAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+  /**
+   * list for item
+   */
   private var listData = listOf<String>()
+  /**
+   * list for displaying banner on top
+   */
   private var bannerList = listOf<String>()
+  private var lastBannerIndex = 0
 
   fun updateList(listUpdate: List<String>) {
     listData = listUpdate
@@ -15,12 +26,12 @@ class SimpleListAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
   fun setBannerList(listUpdate: List<String>) {
     bannerList = listUpdate
-    notifyDataSetChanged()
+    notifyItemChanged(BANNER_POSITION)
   }
 
   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
     return when (viewType) {
-      0 -> {
+      VIEWTYPE_BANNER -> {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_banner, parent, false)
         BannerHolder(view)
       }
@@ -35,19 +46,26 @@ class SimpleListAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
   override fun getItemViewType(position: Int): Int {
     // fist item default = 0 (Banner type)
-    return if (position == 0) 0 else 1
+    return if (position == BANNER_POSITION) VIEWTYPE_BANNER else VIEWTYPE_ITEM
   }
 
   override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
     val viewType = getItemViewType(position)
     val data = listData[position]
     when (viewType) {
-      0 -> {
-        (holder as BannerHolder).bind(bannerList)
+      VIEWTYPE_BANNER -> {
+        (holder as BannerHolder).bind(bannerList, lastBannerIndex)
       }
       else -> {
         (holder as SimpleHolder).bind(data)
       }
     }
+  }
+
+  override fun onViewRecycled(holder: RecyclerView.ViewHolder) {
+    if (holder is BannerHolder) {
+      lastBannerIndex = holder.lastIndex()
+    }
+    super.onViewRecycled(holder)
   }
 }
