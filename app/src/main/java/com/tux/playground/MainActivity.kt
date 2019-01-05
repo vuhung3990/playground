2095b2e539f32f9746b239a3c1947a52a9d97bc4
Hung Vu <vuhung3990@gmail.com>
2019-01-05 10:10:42 +0700

    implement java_emoji

--- app/src/main/java/com/tux/playground/MainActivity.kt
+++ app/src/main/java/com/tux/playground/MainActivity.kt
@@ -2,11 +2,17 @@ package com.tux.playground
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.vdurmont.emoji.EmojiParser
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    val a = "\uD83D\uDC6F\u200D♂️️"
+    val b = EmojiParser.extractEmojis(text.text.toString()).size
+    println(b)
   }
 }
--- app/src/test/java/com/tux/playground/ExampleUnitTest.kt
+++ app/src/test/java/com/tux/playground/ExampleUnitTest.kt
@@ -2,6 +2,9 @@ package com.tux.playground
 
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import java.text.BreakIterator
+import java.text.Normalizer
+
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +13,47 @@ import org.junit.Test
  */
 class ExampleUnitTest {
 
+  fun length(s: String): Int {
+    var n = 0
+    var i = 0
+    while (i < s.length) {
+      val cp = s.codePointAt(i)
+      i += Character.charCount(cp)
+      ++n
+    }
+    return n
+  }
+
+  fun dump(s: String) {
+    var n = 0
+    var i = 0
+    while (i < s.length) {
+      val cp = s.codePointAt(i)
+      val bytes = Character.charCount(cp)
+      i += bytes
+      System.out.printf("[%d] #%dB: U+%X = %s%n",
+          n, bytes, cp, Character.getName(cp))
+      ++n
+    }
+    System.out.printf("Length:%d%n", n)
+  }
+
+  fun printLength(s: String) {
+    val it = BreakIterator.getCharacterInstance()
+    it.setText(s)
+    var count = 0
+    while (it.next() != BreakIterator.DONE) {
+      count++
+    }
+    println("Grapheme length: $count $s")
+  }
+
   @Test
   fun addition_isCorrect() {
-    assertEquals(4, 2 + 2)
+    val emo = 0x1F468
+
+    println(emo)
+    val result: Int = 1
+    assertEquals(1, result)
   }
 }
